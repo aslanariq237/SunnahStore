f35b973e4b8e2f2517422fc559a72805cd7f122e
Naufal sln <aslanariq009@gmail.com>
2022-02-17 09:51:28 +0700

    17 Feb 9:51

---
+++ src/components/views/pages/Dashbord/carousel/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from "./carousel/carousel"
+
+function Dashbord() {
+    return (
+    <React.Fragment>
+        <Carousel/>
+    </React.Fragment>
+    )
+}
+
+export default Dashbord;
